cb96956fc3079a362d6103a39727b7d0afa18a81
MusaBellur <114991207+MusaBellur@users.noreply.github.com>
2024-02-10 10:40:16 +0300

    Migration Eklendi

--- FoodWithCore/FoodWithCore/Models/Context.cs
+++ FoodWithCore/FoodWithCore/Models/Context.cs
@@ -1,6 +1,15 @@
-﻿namespace FoodWithCore.Models
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace FoodWithCore.Models
 {
-    public class Context
+    public class Context : DbContext
     {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=DESKTOP-QE6J6UN\\SQLEXPRESS; database=DbCoreFood; integrated security=true; TrustServerCertificate=true;");
+        }
+        public DbSet<Food> Foods { get; set; }
+        public DbSet<Category> Categories { get; set; }
     }
 }
