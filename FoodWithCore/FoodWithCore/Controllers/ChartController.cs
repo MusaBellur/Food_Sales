22295c5675e377a392e3d4a900e0206cee0d5075
MusaBellur <114991207+MusaBellur@users.noreply.github.com>
2024-02-26 22:05:26 +0300

    Statistics Eklendi
    İstatistik Sayfası Oluşturuldu. Linq Sorguları Kullanılarak İstatistik Sayfası Tamamlandı

--- FoodWithCore/FoodWithCore/Controllers/ChartController.cs
+++ FoodWithCore/FoodWithCore/Controllers/ChartController.cs
@@ -1,5 +1,6 @@
 ﻿using FoodWithCore.Models;
 using Microsoft.AspNetCore.Mvc;
+using System.Runtime.CompilerServices;
 
 namespace FoodWithCore.Controllers
 {
@@ -57,6 +58,50 @@ namespace FoodWithCore.Controllers
                 }).ToList();
             }
             return list2;
+        }
+        public IActionResult Statistics()
+        {
+            Context c = new Context();
+
+            var deger1 = c.Foods.Count();
+            ViewBag.d1 = deger1;
+
+            var deger2 = c.Categories.Count();
+            ViewBag.d2 = deger2;
+
+            var fruitId = c.Categories.Where(x => x.CategoryName == "Fruits").Select(y => y.CategoryID).FirstOrDefault();
+            var deger3 = c.Foods.Where(x => x.CategoryID == fruitId).Count();
+            ViewBag.d3 = deger3;
+
+            var vegetableId = c.Categories.Where(x => x.CategoryName == "Vegetables").Select(y => y.CategoryID).FirstOrDefault();
+            var deger4 = c.Foods.Where(x => x.CategoryID == vegetableId).Count();
+            ViewBag.d4 = deger4;
+            
+            var deger5 = c.Foods.Sum(x => x.Stock);
+            ViewBag.d5 = deger5;
+
+            var deger6 = c.Foods.Where(x => x.CategoryID == c.Categories.Where(y => y.CategoryName == "Grains").Select(z => z.CategoryID).FirstOrDefault()).Count();
+            ViewBag.d6 = deger6;
+            
+            var deger7 = c.Foods.OrderByDescending(x => x.Stock).Select(y => y.Name).FirstOrDefault();
+            ViewBag.d7 = deger7;
+            
+            var deger8 = c.Foods.OrderBy(x => x.Stock).Select(y => y.Name).FirstOrDefault();
+            ViewBag.d8 = deger8;
+            
+            var deger9 = c.Foods.Average(x => x.Price).ToString("0.00");
+            ViewBag.d9 = deger9;
+
+            var deger10 = c.Foods.Where(x => x.CategoryID == fruitId).Sum(y => y.Stock);
+            ViewBag.d10 = deger10;
+            
+            var deger11 = c.Foods.Where(x => x.CategoryID == vegetableId).Sum(y => y.Stock);
+            ViewBag.d11 = deger11;
+
+            var deger12 = c.Foods.OrderByDescending(x => x.Price).Select(y => y.Name).FirstOrDefault();
+            ViewBag.d12 = deger12;
+
+            return View();
         }
     }
 }
