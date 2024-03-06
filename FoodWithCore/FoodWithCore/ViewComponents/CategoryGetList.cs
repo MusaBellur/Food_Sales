194eea1af440b296169f4ac4198e869af078bd49
MusaBellur <114991207+MusaBellur@users.noreply.github.com>
2024-03-06 14:38:17 +0300

    Vitrin Teması Eklendi Ve Düzenlendi
    View Componentler Hazırlandı.  Kategoriler Ve Gıdalar Vitrinde Listelendi

---
+++ FoodWithCore/FoodWithCore/ViewComponents/CategoryGetList.cs
@@ -0,0 +1,15 @@
+﻿using CoreAndFood.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FoodWithCore.ViewComponents
+{
+    public class CategoryGetList:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            CategoryRepository repository = new CategoryRepository();
+            var categorylist = repository.TList();
+            return View(categorylist);
+        }
+    }
+}
---
+++ FoodWithCore/FoodWithCore/ViewComponents/FoodGetList.cs
@@ -0,0 +1,15 @@
+﻿using CoreAndFood.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FoodWithCore.ViewComponents
+{
+	public class FoodGetList:ViewComponent
+	{
+		public IViewComponentResult Invoke()
+		{
+			FoodRepository repository = new FoodRepository();
+			var foodlist = repository.TList();
+			return View(foodlist);
+		}
+	}
+}
